bd66cbab5c6b1ad1d7dc57ca67ee80ed095c510a
PedroBaraunaProgrammer <47827453+PedroBaraunaProgrammer@users.noreply.github.com>
2019-02-20 13:55:11 -0400

    first programe in C
    get a number and print this number on screen

---
+++ firstProgame.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main ()
+{
+	//start the variable num that is a type int
+	int num;
+
+	//print a message of text asking to write a number and get this number
+	printf("enter a number");
+	scanf("%d",&num);
+
+	//print the number entere
+	printf("you enter the number %d",num);
+}
